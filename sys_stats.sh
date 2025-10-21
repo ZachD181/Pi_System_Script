8c2abed085c0052540b3b4e72c6686cf0b6263e0
ZachD181 <84939280+ZachD181@users.noreply.github.com>
2025-10-21 12:17:53 -0600

    Fix typo in comment for SATA devices

--- sys_stats.sh
+++ sys_stats.sh
@@ -39,6 +39,6 @@
     
     
 # Output the following information to a file called sata
-    # 1. Any Sata devices connected to the machine along with human readable sizes of the device
+    # 1. Any Sata devices connected to the machine along with human readable sizes of the devices
     # 2. More specific information about each device that is connected to the machine
     
